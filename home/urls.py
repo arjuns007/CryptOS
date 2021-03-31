e02cb3af6e337fedb15de930078142c77a1808df
Aman Mehra <65860158+Dru444@users.noreply.github.com>
2021-04-01 00:27:56 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,10 @@ from . import views
 urlpatterns = [
    
     path('about',views.about, name='about'),
+    path('about',views.about, name='about'),
+    path('cal_con',views.cal_con, name='cal_con'),
+    path('',views.home, name='home'),
+    path('faq',views.faq, name='faq'),
+    path('blog',views.blog, name='blog'),
    
 ]
