b8484321eda88f9cc433b6209ad28d13eaf7c1f1
Dru444 <aman.mehra18@st.niituniversity.in>
2021-03-31 23:24:47 +0530

    Revised(Django)

---
+++ home/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+import requests
+
+# Create your views here.
--- positions/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class PositionsConfig(AppConfig):
-    name = 'positions'
-
-    def ready(self):
-        import positions.signals
-
