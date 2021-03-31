60dfa8a1b2463c4d2ed3b08d5b08bf9586787b44
divtejbindra <50200534+divtejbindra@users.noreply.github.com>
2021-04-01 01:18:35 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-   
-    path('about',views.about, name='about'),
     path('about',views.about, name='about'),
     path('cal_con',views.cal_con, name='cal_con'),
-    path('',views.home, name='home'),
+    path('guide',views.guide, name='guide'),
     path('faq',views.faq, name='faq'),
     path('blog',views.blog, name='blog'),
    
