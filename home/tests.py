dfcbd0af576cc87ca2cd6f26093090e051aaf155
Aman Mehra <65860158+Dru444@users.noreply.github.com>
2021-12-18 18:35:05 +0530

    Added tests.py

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+import unittest
+import requests
+from . import models
 
-# Create your tests here.
+class DataTest(unittest.TestCase):
+                 
+    def test_1(self):
+        
+        url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+        data = requests.get(url).json()
+        self.assertEqual(data[0]["name"],"Bitcoin")
+
+    def test_2(self):
+        url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+        data = requests.get(url).json()
+        self.assertEqual(data[0]["symbol"],"btc")
+
+    def test_3(self):
+        url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+        data = requests.get(url).json()
+        self.assertEqual(data[1]["symbol"],"btc")
+        
+
